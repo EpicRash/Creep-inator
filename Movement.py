806a70e15d7c2f53139223b82c8b7b2a19e397a5
Angel <58895837+Omen5@users.noreply.github.com>
2022-01-05 11:42:25 -0500

    Added keystroke capture

--- Movement.py
+++ Movement.py
@@ -1,9 +1,28 @@
 from djitellopy import tello
+import pygame
 from time import sleep
 
 me = tello.Tello
 me.connect()
 print(me.getbattery())
+
+def init():
+    pygame.init()
+    win=pygame.display.set_mode((400,400))
+
+def getkey(keyName):
+    ans = False
+    for eve in pygame.event.get(): pass
+    keyInput = pygame.key.get_pressed()
+    myKey = getattr(pygame, 'K_{}'.format(keyName))
+    if keyInput[myKey]:
+        ans = True
+    pygame.display.update
+
+    return ans
+
+if __name__ == '__main__':
+    init()
 
 
 
